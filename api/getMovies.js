152eac5ddd0cf7d9b651c89689932a554587b5f7
Pedro Freitas <pedro.freitas@dbccompany.com.br>
2023-03-07 16:48:19 -0300

    teste

--- api/getMovies.js
+++ api/getMovies.js
@@ -1,5 +1,24 @@
+app.use(cors());
+
 module.exports = (req, res) => {
-    res.json({
-        hola: 'mundo'    
-    })
-}
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  res.json({
+    hola: "mundo",
+  });
+};
